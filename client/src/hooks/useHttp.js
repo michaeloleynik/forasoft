0dceff51bf3b458273a50bc92b37b661249d2482
michaeloleynik <mihasick2014@gmail.com>
2021-11-25 11:52:50 +0300

    deleted unnesessery code in useHttp

--- client/src/hooks/useHttp.js
+++ client/src/hooks/useHttp.js
@@ -16,10 +16,6 @@ export const useHttp = () => {
       const response = await fetch(url, {method, body, headers});
       const data = await response.json();
 
-      // if (!response.ok) {
-      //   throw new Error(data.message || 'Something went wrong...');
-      // }
-
       setLoading(false);
 
       return data;
